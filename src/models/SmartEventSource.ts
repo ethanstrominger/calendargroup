10c2e011d7bd133df2cdfb698bde922e6e0fd193
ethanstrominger <ethanstrominger2@gmail.com>
2021-04-06 22:00:39 -0400

    Updated SmartEventSource and test cases

--- src/models/SmartEventSource.ts
+++ src/models/SmartEventSource.ts
@@ -6,36 +6,31 @@ export interface ISmartEventSource {
 }
 
 export class SmartEventSource implements ISmartEventSource {
-  static smartEventSources: { [key: string]: SmartEventSource } = {};
-  static getByUUID(uuid: string) {
-    return SmartEventSource.smartEventSources[uuid];
-  }
+
   // C Field - 
-  _aggregatedEvents: { [key: string]: AggregatedEvent } = {};
-  _uuid: string = "";
+  readonly aggregatedEvents: { [key: string]: AggregatedEvent } = {};
+  readonly uuid: string = "";
   name: string;
   sourceType: string;
   source: string;
 
+  static smartEventSources: { [key: string]: SmartEventSource } = {};
+  static getByUUID(uuid: string) {
+    return SmartEventSource.smartEventSources[uuid];
+  }
+
   constructor(name, sourceType, source) {
-    this._uuid = uuidv4();
+    this.uuid = uuidv4();
     this.name = name;
     this.sourceType = sourceType;
     this.source = source;
-    SmartEventSource.smartEventSources[this._uuid] = this;
+    SmartEventSource.smartEventSources[this.uuid] = this;
   }
-
-
-  get uuid(): string {return this._uuid}
 
   addAggregatedEvent = (aggregatedEvent: AggregatedEvent) =>
-    (this._aggregatedEvents[
+    (this.aggregatedEvents[
       aggregatedEvent.uniqueOccurenceId
     ] = aggregatedEvent);
-
-  get aggregatedEvents() {
-    return this._aggregatedEvents;
-  }
 
   addAggregatedEvents = (aggregatedEvents: AggregatedEvent[]) =>
     aggregatedEvents.map((aggregatedEvent) => {
