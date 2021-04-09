da417701865cc66e1be1529b976fc1e654f2135a
ethanstrominger <ethanstrominger2@gmail.com>
2021-04-08 23:26:16 -0400

    Commit old code before redesign of date

---
+++ src/models/__test__/AggEventSource.test.js
@@ -0,0 +1,74 @@
+import { AggEventSource } from "../AggEventSource";
+import { AggEvent } from "../AggEvent";
+describe("AggEventSource CRUD", () => {
+    it("A aggEventSource can be created", () => {
+        const name = "Name";
+        const sourceType = "URL";
+        const source = "www.google.com";
+        const aggEventSource = new AggEventSource(name, sourceType, source);
+        expect(aggEventSource).toBeDefined();
+        expect(aggEventSource.name).toEqual(name);
+        expect(aggEventSource.sourceType).toEqual(sourceType);
+        expect(aggEventSource.source).toEqual(source);
+    });
+    it("You can get an aggEventSource by UUID", () => {
+        const name = "Name";
+        const sourceType = "URL";
+        const source = "www.google.com";
+        const aggEventSource = new AggEventSource(name, sourceType, source);
+        const retrievedEventSource = AggEventSource.getByUUID(aggEventSource.uuid);
+        expect(retrievedEventSource.name).toEqual(name);
+        expect(retrievedEventSource.sourceType).toEqual(sourceType);
+        expect(retrievedEventSource.source).toEqual(source);
+    });
+    it("Events can be added to a aggEventSource", () => {
+        const currentTime = new Date();
+        const aggEvent1 = new AggEvent({
+            uid: "1",
+            aggEventWebcalId: "a webcalid",
+            title: "a title",
+            description: "a description",
+            startDateTime: currentTime,
+            endDateTime: new Date(currentTime.getTime() + 1000 * 60 * 60),
+        });
+        const tomorrowTime = new Date(currentTime.getTime() + 1000 * 60 * 60 * 24);
+        const aggEvent2 = new AggEvent({
+            uid: "2",
+            aggEventWebcalId: "a webcalid for event 2",
+            title: "a title for event 2",
+            description: "a description for event 2",
+            startDateTime: currentTime,
+            endDateTime: new Date(currentTime.getTime() + 1000 * 60 * 60),
+        });
+        const aggEventSource = new AggEventSource();
+        aggEventSource.addAggEvent(aggEvent1);
+        aggEventSource.addAggEvent(aggEvent2);
+        expect(aggEvent1 === aggEventSource.aggEvents["1"]).toBeTruthy();
+        expect(aggEvent2 === aggEventSource.aggEvents["2"]).toBeTruthy();
+    });
+    it("An array of events can be added to a aggEventSource", () => {
+        const currentTime = new Date();
+        const aggEvent1 = new AggEvent({
+            uid: "1",
+            aggEventWebcalId: "a webcalid",
+            title: "a title",
+            description: "a description",
+            startDateTime: currentTime,
+            endDateTime: new Date(currentTime.getTime() + 1000 * 60 * 60),
+        });
+        const tomorrowTime = new Date(currentTime.getTime() + 1000 * 60 * 60 * 24);
+        const aggEvent2 = new AggEvent({
+            uid: "2",
+            aggEventWebcalId: "a webcalid for event 2",
+            title: "a title for event 2",
+            description: "a description for event 2",
+            startDateTime: currentTime,
+            endDateTime: new Date(currentTime.getTime() + 1000 * 60 * 60),
+        });
+        const aggEventsArray = [aggEvent1, aggEvent2];
+        const aggEventSource = new AggEventSource();
+        aggEventSource.addAggEvents(aggEventsArray);
+        expect(aggEvent1 === aggEventSource.aggEvents["1"]).toBeTruthy();
+        expect(aggEvent2 === aggEventSource.aggEvents["2"]).toBeTruthy();
+    });
+});
