4db4472306b082bfba7a4c32033f24c007f8e884
ethanstrominger <ethanstrominger2@gmail.com>
2021-05-18 09:04:53 -0400

    fixed merge issues

--- src/IcalObject.ts
+++ src/IcalObject.ts
@@ -3,6 +3,6 @@ import { AggEvent } from "./models/AggEvent";
 export class IcalObject {
   defaultTimezoneId: string;
   constructor() {}
-  events: [AggEvent];
-  timezoneIds: [string];
+  events: AggEvent[];
+  timezoneIds: string[];
 }
--- src/IcalUtils.ts
+++ src/IcalUtils.ts
@@ -3,24 +3,25 @@ import { IcalObject } from "./IcalObject";
 import { sync } from "node-ical";
 
 export function getIcalObjectFromText(icalText: string): IcalObject {
-  const icalData = sync.parseICS(icalText);
+  const icalDataStructured = sync.parseICS(icalText);
   let icalLines = icalText.split(/\r?\n/);
 
   const icalObject = new IcalObject();
 
   for (const line of icalLines) {
-    if (line.substring(0,13) == 'X-WR-TIMEZONE') {
+    if (line.substring(0, 13) == "X-WR-TIMEZONE") {
       icalObject.defaultTimezoneId = line.substr(14);
       break;
     }
   }
 
   let timezoneIds = [] as string[];
-  for (const timeZone of Object.values(icalData)) {
-    if (timeZone.tzid) {
-      timezoneIds.push(timeZone.tzid)
+  for (const timeZone of Object.values(icalDataStructured)) {
+    const tzid: string = timeZone["tzid"];
+    if (tzid) {
+      timezoneIds.push(tzid);
     }
-  };
+  }
 
   icalObject.timezoneIds = timezoneIds;
   return icalObject;
