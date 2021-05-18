a06df39812aa60d11b281d27c0fcc25902b8369f
ethanstrominger <ethanstrominger2@gmail.com>
2021-05-18 17:00:20 -0400

    Removed unused code, documented sample outputs

--- src/IcalObject.ts
+++ src/IcalObject.ts
@@ -3,5 +3,4 @@ import { AggEvent } from "./models/AggEvent";
 export class IcalObject {
   constructor() {}
   events: AggEvent[];
-  timezoneIds: string[];
 }
--- src/IcalUtils.ts
+++ src/IcalUtils.ts
@@ -5,25 +5,5 @@ export function getIcalObjectFromText(icalText: string): IcalObject {
   const icalData = sync.parseICS(icalText);
   const icalObject = new IcalObject();
 
-  let timezoneIds = [] as string[];
-
-  icalObject.timezoneIds = timezoneIds;
   return icalObject;
-}
-
-export function getIcalObjectFromText2(icalText: string): IcalObject {
-  console.log(icalText);
-  const parsedCal = sync.parseICS(icalText);
-  console.log("parsedCal", parsedCal);
-  const calObjects = Object.values(parsedCal);
-  console.log("calObjects", calObjects);
-  // todo:
-  // 1. Initialize new icalObject that will be returned.
-  // 2. For each object in calObjects where type="VTIMEZONE", add the timezoneid to the
-  //    icalObject.timezoneids array.
-  // 3. return the icalObject (instead of the temporary code below)
-  // 4. rename sample files "new *.txt"
-
-  const result = getIcalObjectFromText(icalText);
-  return result;
 }
